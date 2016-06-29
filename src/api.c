61742d6b8829068aa93f6fb4e442d581b2881446
Junde Yhi <lmy441900@gmail.com>
2016-06-29 17:12:49 +0800

    add: api.c: _gank_io_api_sorted_url_form
    Signed-off-by: Junde Yhi <lmy441900@gmail.com>

--- src/api.c
+++ src/api.c
@@ -43,9 +43,44 @@ int _gank_io_api_daily_url_form (char **url, unsigned int year, unsigned int mon
 
 
 
-int _gank_io_api_sorted_url_form (char **url, unsigned int nRequest, unsigned int nPage)
+int _gank_io_api_sorted_url_form (char **url, GankIoResType resType, unsigned int nRequest, unsigned int nPage)
 {
+    char *buf = gank_io_xmalloc (BUFFER_SIZE);
+    char strResType = gank_io_xmalloc (BUFFER_SIZE);
+    int retVal = 0;
 
+    switch (resType) {
+        case Goods:
+            strncpy (strResType, "福利", BUFFER_SIZE);
+            break;
+        case Android:
+            strncpy (strResType, "Android", BUFFER_SIZE);
+            break;
+        case Ios:
+            strncpy (strResType, "iOS", BUFFER_SIZE);
+            break;
+        case RelaxingMovies:
+            strncpy (strResType, "休息视频", BUFFER_SIZE);
+            break;
+        case ExtendRes:
+            strncpy (strResType, "拓展资源", BUFFER_SIZE);
+            break;
+        case Frontend:
+            strncpy (strResType, "前端", BUFFER_SIZE);
+            break;
+        default:
+            // WTF
+            gank_io_error ("%s:%d Unexpected: Wrong resType", __FILE__, __LINE__);
+            break;
+    }
+
+    retVal = snprintf (BUFFER_SIZE, "%s/%s/%d/%d", BaseUrl, strResType, nRequest, nPage);
+    if (retVal <= BUFFER_SIZE) {
+        *url = buf;
+        return;
+    } else {
+        gank_io_error ("%s:%d String overflowed!", __FILE__, __LINE__);
+    }
 }
 
 
