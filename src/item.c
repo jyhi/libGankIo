5db56f077723c867d9cfda1ae91975f865c4997b
Junde Yhi <lmy441900@gmail.com>
2016-07-11 20:15:13 +0800

    add: item: prototype parameters
    Signed-off-by: Junde Yhi <lmy441900@gmail.com>

--- src/item.c
+++ src/item.c
@@ -16,22 +16,25 @@
  * License along with libGankIo.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _GANK_IO_ITEM_H
-#define _GANK_IO_ITEM_H
+int gank_io_item_info_fetch (GankIoItem **items, const unsigned int nItem)
+{
 
-/* gank_io_item_info_fetch: Fetch items of specific numbers, and write information into the specific 'GankIoItem' items.
- * NOTE: Unlike gank_io_item_content_fetch, this function only fetches summaries.
- */
-int gank_io_item_info_fetch (GankIoItem **items, const unsigned int nItem);
+}
 
-/* gank_io_item_content_fetch: Fetch the content of the specific item, which contains information fetched by
- *   gank_io_item_info_fetch.
- */
-int gank_io_item_content_fetch (GankIoItem *item);
 
-/* gank_io_item_free: Free the specific item.
- * NOTE: If the item is already NULL, this function does nothing.
- */
-int gank_io_item_free (GankIoItem *item);
 
-#endif /* end of include guard: _GANK_IO_ITEM_H */
+
+
+int gank_io_item_content_fetch (GankIoItem *item)
+{
+
+}
+
+
+
+
+
+int gank_io_item_free (GankIoItem *item)
+{
+
+}
